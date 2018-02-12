0b2b4b7fa1175a812f42d1ec407663b4e9b3d8d1
Erik Cederlöf <Erik.Cederlof@evry.com>
2018-02-12 20:09:29 +0100

    make into a package with separate python-script file

--- python_scripts/car_heater.py
+++ python_scripts/car_heater.py
@@ -4,9 +4,10 @@ temperature_sensor_id = "sensor.27_temperature"
 
 #get or create enable-state for automation
 heater_enable_boolean = "input_boolean.enable_heater_automation";
-
+#logger.warning("CAR_HEATER {}".format(1))
 #if enabled
 if hass.states.get(heater_enable_boolean).state is 'on':
+#	logger.warning("CAR_HEATER {}".format(2))
 	now = datetime.datetime.now()
 
 	#variables to set from gui
@@ -19,6 +20,7 @@ if hass.states.get(heater_enable_boolean).state is 'on':
 	if departure_time < now:
 		departure_time = departure_time + datetime.timedelta(days = 1)
 
+#	logger.warning("CAR_HEATER {}".format(3))
 	hass.states.set('sensor.heater_departure_time', departure_time, {
 		'friendly_name': 'Avresetid',
 		'icon': 'car,'
@@ -30,11 +32,14 @@ if hass.states.get(heater_enable_boolean).state is 'on':
 	#fetch the temperature from the sensor
 	temperature = hass.states.get(temperature_sensor_id).state
 
+#	logger.warning("CAR_HEATER {} temp {} isfloat {}".format(4, temperature, temperature.lstrip('-').replace('.','',1).isdigit()))
 	#check that we are below the max temp for using the heater
-	if float(temperature) <= float(max_temperature):
+	if temperature.lstrip('-').replace('.','',1).isdigit() and float(temperature) <= float(max_temperature):
+#		logger.warning("CAR_HEATER {}".format(5))
 		heater_switch_status_states = hass.states.get(heater_switch_id)
 		heater_switch_status = heater_switch_status_states.state
 
+#		logger.warning("CAR_HEATER {}".format(6))
 		#how early should we start the heater
 		temperature_float = float(temperature)
 		if temperature_float <= -10:
@@ -47,6 +52,7 @@ if hass.states.get(heater_enable_boolean).state is 'on':
 
 		heater_should_be_on = start_heater_time < now and departure_time > now
 
+#		logger.warning("CAR_HEATER {}".format(7))
 		#toggle the heater switch
 		if heater_switch_status is 'off' and heater_should_be_on:
 			hass.services.call('switch', 'turn_on', {'entity_id': heater_switch_id})
@@ -55,8 +61,11 @@ if hass.states.get(heater_enable_boolean).state is 'on':
 			hass.services.call('switch', 'turn_off', {'entity_id': heater_switch_id})
 			hass.services.call('notify','telegram', {"message": "Motorvärmaren stängs av."})
 
+#		logger.warning("CAR_HEATER {}".format(8))
 		hass.states.set('sensor.heater_will_run_when', start_heater_time, {
 			'friendly_name': 'Motorvärmare startas',
 			'icon': 'mdi:av-timer',
 		})
 		#logger.warning("sensor heater_will_run_when {}".format(start_heater_time))
+#	else:
+#		logger.warning("CAR_HEATER {}".format(9))
