6fe9a3ab6311f449a90b7a9104d8c5124eb6dabd
Erik Cederlöf <Erik.Cederlof@evry.com>
2019-01-17 20:34:03 +0100

    Initial add files

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# home-assistant-car-heater
+# Home Assistant Car Heater
+
+Set your departure time the night before you're leaving the house, and the heater will be run 1 or 2 hours before departure, depending on the outside temperature.
+
+With inspiration from https://github.com/Gnaget2/Car-Heater-Package which uses yaml for everything, which I found painful. The heater logic is instead performed by a python script `python_scripts/car_heater.py`.
+
+I haven't had the time to make this into a proper HASS-package, but hopefully it can be used as inspiration.
+
+Notes:
+* `switch.z2_switch_switch` is the name of my switch controlling the heater (from the wall-outlet)
+* `sensor.27_temperature` is the name of the thermometer measuring outside temperature
+* At the moment:
+   * Less than -10 Celcius heater will start 2 hours before departure
+   * Less than 0 Celcius heater will start 1 hour before departure
+   * Less than +10 Celcius heater will start 30 minutes before departure
+* I use Telegram for notifications
+
+---
+
+<img src="images/screenshot.png?raw=true" align="left" width="300px" >
---
+++ python_scripts/car_heater.py
@@ -0,0 +1,76 @@
+#logger.warning("running car_heater.py")
+#variables to modify in script
+heater_switch_id = "switch.z2_switch_switch"
+temperature_sensor_id = "sensor.27_temperature"
+
+#get or create enable-state for automation
+heater_enable_boolean = "input_boolean.enable_heater_automation";
+#logger.warning("CAR_HEATER {}".format(1))
+#if enabled
+
+if hass.states.get(temperature_sensor_id) is None:
+	logger.error("Cannot find temperature sensor with entity_id: \"{}\"".format(temperature_sensor_id))
+else:
+	if hass.states.get(heater_enable_boolean).state is 'on':
+	#	logger.warning("CAR_HEATER {}".format(2))
+		now = datetime.datetime.now()
+
+		#variables to set from gui
+		departure_time_input = hass.states.get("input_datetime.heater_departure_time_input")
+		departure_time_hours = int(departure_time_input.attributes["hour"])
+		departure_time_minutes = int(departure_time_input.attributes["minute"])
+
+		#we're assuming lift off is during todays date, will be problematic around midnight..
+		departure_time = datetime.datetime(now.year, now.month, now.day, departure_time_hours, departure_time_minutes, 0)
+		if departure_time < now:
+			departure_time = departure_time + datetime.timedelta(days = 1)
+
+	#	logger.warning("CAR_HEATER {}".format(3))
+		hass.states.set('sensor.heater_departure_time', departure_time, {
+			'friendly_name': 'Avresetid',
+			'icon': 'car,'
+		})
+
+		#stop using the heater above this temperature
+		max_temperature = 10
+
+		#fetch the temperature from the sensor
+		temperature = hass.states.get(temperature_sensor_id).state
+
+		#logger.warning("CAR_HEATER {} temp {} obj {} ".format(4, temperature, temperature_sensor_id))
+		#check that we are below the max temp for using the heater
+		if temperature.lstrip('-').replace('.','',1).isdigit() and float(temperature) <= float(max_temperature):
+	#		logger.warning("CAR_HEATER {}".format(5))
+			heater_switch_status_states = hass.states.get(heater_switch_id)
+			heater_switch_status = heater_switch_status_states.state
+
+	#		logger.warning("CAR_HEATER {}".format(6))
+			#how early should we start the heater
+			temperature_float = float(temperature)
+			if temperature_float <= -10:
+				delta = datetime.timedelta(hours = 2)
+			elif temperature_float <= 0:
+				delta = datetime.timedelta(hours = 1)
+			elif temperature_float <= max_temperature:
+				delta = datetime.timedelta(minutes = 30)
+			start_heater_time = departure_time - delta
+
+			heater_should_be_on = start_heater_time < now and departure_time > now
+
+	#		logger.warning("CAR_HEATER {}".format(7))
+			#toggle the heater switch
+			if heater_switch_status is 'off' and heater_should_be_on:
+				hass.services.call('switch', 'turn_on', {'entity_id': heater_switch_id})
+				hass.services.call('notify','telegram', {"message": "Motorvärmaren startas."})
+			if heater_switch_status is 'on' and not heater_should_be_on:
+				hass.services.call('switch', 'turn_off', {'entity_id': heater_switch_id})
+				hass.services.call('notify','telegram', {"message": "Motorvärmaren stängs av."})
+
+	#		logger.warning("CAR_HEATER {}".format(8))
+			hass.states.set('sensor.heater_will_run_when', start_heater_time, {
+				'friendly_name': 'Motorvärmare startas',
+				'icon': 'mdi:av-timer',
+			})
+			#logger.warning("sensor heater_will_run_when {}".format(start_heater_time))
+	#	else:
+	#		logger.warning("CAR_HEATER {}".format(9))
